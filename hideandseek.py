700507af60083b2a4611028a92869ee72a1d84a1
SwiftGlitxh <72777943+SwiftGlitxh@users.noreply.github.com>
2023-06-25 15:09:50 +0100

    Update hideandseek.py

--- hideandseek.py
+++ hideandseek.py
@@ -8,6 +8,8 @@ import argparse
 
 parser = argparse.ArgumentParser(description="HideAndSeek Flags")
 parser.add_argument('-v', '--verbose', dest="verbose", action='store_true', help='Verbose')
+parser.add_argument('-o', '--output', dest="output_file", metavar="FILE", help='Output file path')
+parser.add_argument('-s', '--silent', dest="silent", action='store_true', help='Silent mode')
 
 args = parser.parse_args()
 input_string = input("[+] Enter a username: ")
@@ -27,7 +29,7 @@ def banner():
   ▒ ░▒░ ░ ▒ ░ ░ ▒  ▒  ░ ░  ░ ▒   ▒▒ ░░ ░░   ░ ▒░ ░ ▒  ▒ ░ ░▒  ░ ░ ░ ░  ░ ░ ░  ░░ ░▒ ▒░
   ░  ░░ ░ ▒ ░ ░ ░  ░    ░    ░   ▒      ░   ░ ░  ░ ░  ░ ░  ░  ░     ░      ░   ░ ░░ ░ 
   ░  ░  ░ ░     ░       ░  ░     ░  ░         ░    ░          ░     ░  ░   ░  ░░  ░   
-              ░                                  ░                                    \033[32m { Version 0.7 }\033[37m
+              ░                                  ░                                    \033[32m { Version 0.8 }\033[37m
                     Made with ❤️ by \033[33m SwiftGlitxh \033[37m\n
   [=]   \033[35m HideAndSeek is a small script to scan websites for a user \033[37m       [=]
   [=]   \033[36m             https://twitter.com/swiftglitxh                  \033[37m    [=]
@@ -40,12 +42,27 @@ def scan_website(website):
         response = requests.get(website + input_string, timeout=3)
         if response.status_code == 200:
             if input_string in response.text:
-                print(f"[\033[32m+\033[37m] [FOUND] \033[36m {website}{input_string}\033[33m  => \033[32m {input_string}\033[37m Found!")
+                output = f"[\033[32m+\033[37m] [FOUND] \033[36m {website}{input_string}\033[33m  => \033[32m {input_string}\033[37m Found!"
             elif args.verbose:
-                print(f"[\033[31m+\033[37m] [NO USER] \033[36m{website}{input_string}\033[33m  => \033[32m {input_string}\033[37m No User Found!")
+                output = f"[\033[31m+\033[37m] [NO USER] \033[36m{website}{input_string}\033[33m  => \033[32m {input_string}\033[37m No User Found!"
+            else:
+                return
+        else:
+            return
     except requests.exceptions.RequestException:
         if args.verbose:
-            print(f"[\033[31m+\033[37m] [DOWN] \033[36m {website}{input_string}\033[31m is down\033[37m")
+            output = f"[\033[31m+\033[37m] [DOWN] \033[36m {website}{input_string}\033[31m is down\033[37m"
+        else:
+            return
+
+    if args.silent:
+        return
+
+    if args.output_file:
+        with open(args.output_file, 'a') as file:
+            file.write(output + '\n')
+    else:
+        print(output)
 
 def main():
     banner()
