a10d3f48f751304ebd93dae7dd60786414643908
SwiftGlitxh <72777943+SwiftGlitxh@users.noreply.github.com>
2023-06-27 19:54:28 +0100

    Update hideandseek.py

--- hideandseek.py
+++ hideandseek.py
@@ -5,6 +5,7 @@ import concurrent.futures
 import requests
 from links import websites
 import argparse
+import pickle 
 
 try:
     import fake_useragent
@@ -14,11 +15,9 @@ except ImportError:
 
 parser = argparse.ArgumentParser(description="HideAndSeek Flags")
 parser.add_argument('-v', '--verbose', dest="verbose", action='store_true', help='Verbose')
-parser.add_argument('-o', '--output', dest="output_file", metavar="FILE", help='Output file path')
-parser.add_argument('-s', '--silent', dest="silent", action='store_true', help='Silent mode')
+parser.add_argument('-s', '--store', dest="output_file", action='store_true', help='Output file path')
 
 args = parser.parse_args()
-input_string = input("[+] Enter a username: ")
 os.system('clear')
 
 def install_fake_useragent():
@@ -35,15 +34,14 @@ def banner():
   ▒ ░▒░ ░ ▒ ░ ░ ▒  ▒  ░ ░  ░ ▒   ▒▒ ░░ ░░   ░ ▒░ ░ ▒  ▒ ░ ░▒  ░ ░ ░ ░  ░ ░ ░  ░░ ░▒ ▒░
   ░  ░░ ░ ▒ ░ ░ ░  ░    ░    ░   ▒      ░   ░ ░  ░ ░  ░ ░  ░  ░     ░      ░   ░ ░░ ░ 
   ░  ░  ░ ░     ░       ░  ░     ░  ░         ░    ░          ░     ░  ░   ░  ░░  ░   
-              ░                                  ░                                    \033[32m { Version 0.6 }\033[37m
+              ░                                  ░                                    \033[32m { Version 0.7 }\033[37m
                     Made with ❤️ by \033[33m SwiftGlitxh \033[37m\n
   [=]   \033[35m HideAndSeek is a small script to scan websites for a user \033[37m       [=]
   [=]   \033[36m             https://twitter.com/swiftglitxh                  \033[37m    [=]
   [=]   \033[35m   *** Please note not all sites will be 100% correct ***     \033[37m    [=]
 ''')
-    print(f"  [\033[33m!\033[37m]\033[33m  Scanning '\033[32m{input_string}\033[33m' on {len(websites)} different platforms\033[37m\n")
 
-def scan_website(website):
+def scan_website(website, input_string):
     try:
         user_agent = str(fake_useragent.UserAgent()) if fake_useragent_installed else None
         response = requests.get(website + input_string, timeout=3, headers={'User-Agent': user_agent})
@@ -64,15 +62,54 @@ def scan_website(website):
         else:
             return
 
-    if args.silent:
-        return
-
     if args.output_file:
-        with open(args.output_file, 'a') as file:
-            file.write(output + '\n')
+        store_output(website, input_string)
         print(output)
     else:
         print(output)
+
+
+def store_output(website, input_string):
+    if os.path.exists("database.pickle"):
+        with open("database.pickle", "rb") as file:
+            database = pickle.load(file)
+    else:
+        database = []
+
+    database.append((website, input_string))
+
+    with open("database.pickle", "wb") as file:
+        pickle.dump(database, file)
+
+
+def show_database():
+    if os.path.exists("database.pickle"):
+        with open("database.pickle", "rb") as file:
+            database = pickle.load(file)
+
+        print("Stored Database:")
+        print("-" * 70)
+        for entry in database:
+            print(entry[0], entry[1])
+        print("-" * 70)
+    else:
+        print("No database found.")
+
+    input("\nPress any key to continue....")
+    os.system("clear")
+    banner()
+
+def destroy_database():
+    if os.path.exists("database.pickle"):
+        os.remove("database.pickle")
+        print("Database destroyed successfully.")
+    else:
+        print("No database found.")
+
+    input("\nPress any key to continue....")
+    os.system("clear")
+    banner()
+
 
 def main():
     if not fake_useragent_installed:
@@ -89,8 +126,34 @@ def main():
             return
 
     banner()
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        executor.map(scan_website, websites)
+
+    while True:
+        print("  [1] Scan Websites For Username")
+        print("  [2] Show Collected Database")
+        print("  [3] Destroy Database")
+        print("  [0] Exit")
+        choice = input("\nEnter your choice: ")
+
+        if choice == "1":
+            input_string = input("[+] Enter a username: ")
+            print(f'\n\033[1;33m[+] Scanning for {input_string}...\033[0;37m')
+            print('-'*70)
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                executor.map(scan_website, websites, [input_string] * len(websites))
+            input("\nPress any key to continue....")
+            os.system('clear')
+            banner()    
+        elif choice == "2":
+            show_database()   
+        elif choice == "3":
+            destroy_database()     
+        elif choice == "0":
+            break
+        else:
+            print("\n[!] Invalid choice. Please try again.")
+            input("\nPress any key to continue....")
+            os.system('clear')
+            banner()
 
 if __name__ == "__main__":
     main()
